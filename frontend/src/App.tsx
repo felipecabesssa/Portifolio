ba354d9b88a0d59226889943d23131d6fb87009e
felipecabesssa <felipesoares100@hotmail.com>
2021-09-08 01:11:56 -0300

    Tabela com Formações e Cursos

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,5 @@
+import DataTable from "components/DataTable";
+import Footer from "components/Footer";
 import NavBar from "components/navBar";
 
 function App() {
@@ -5,8 +7,10 @@ function App() {
     <>
       <NavBar />
       <div className="container">
-        <h1>Olá Mundo !</h1>
+        <h1>Formações e Cursos.</h1>
+        <DataTable />
       </div>
+      <Footer />
     </>
   );
 }
--- frontend/src/components/navBar/index.tsx
+++ frontend/src/components/navBar/index.tsx
@@ -5,17 +5,17 @@ const Navbar = () => {
 
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">Projetos</a>
+                <a className="espaco" aria-current="page" href="#"><img src={ImgDsDark} alt="DevSuperior" width="120" /></a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <img src={ImgDsDark} alt="DevSuperior" width="120" className="nav-link active" aria-current="page" href="#" /></img>
+                            <a className="nav-link active" href="#">Projetos</a>               
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#">formações</a>
+                            <a className="nav-link active" aria-current="page" href="#">Formações</a>
                         </li>
                         <li className="nav-item">
                             <a className="nav-link" href="#">Cursos</a>
@@ -25,8 +25,8 @@ const Navbar = () => {
                         </li>
                     </ul>
                     <form className="d-flex">
-                        <input className="form-control me-2 button" type="search" placeholder="Search" aria-label="Search" />
-                        <button className="btn btn-outline-success button" type ="submit">Search</button>
+                        <input className="form-control me-2 button1" type="search" placeholder="Procure por linguagem" aria-label="Search" />
+                        <button className="btn btn-outline-success button" type ="submit">Procure</button>
                     </form>
                 </div>
             </div>
