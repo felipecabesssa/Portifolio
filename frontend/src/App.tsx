28c9b5c64b5adb5d009379e5a2b89733bb88fef3
felipecabesssa <felipesoares100@hotmail.com>
2021-09-08 02:26:25 -0300

    Graficos com dados státicos

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,6 @@
+import BarChart from "components/BarChart";
 import DataTable from "components/DataTable";
+import DonutChart from "components/DonutChart";
 import Footer from "components/Footer";
 import NavBar from "components/navBar";
 
@@ -7,9 +9,27 @@ function App() {
     <>
       <NavBar />
       <div className="container">
-        <h1>Formações e Cursos.</h1>
+        <h1 className="text-primary py-3">Dashboard de Projetos - Estudos</h1>
+        <div className="row px-3">
+
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Percentual de conclusão (%)</h5>
+            <BarChart />
+          </div>
+
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Total de horas cursadas</h5>
+            <DonutChart />
+          </div>
+        </div>
+
+        <div className="py-3">
+          <h2 className="text-primary">Todos os Cursos e Formações</h2>
+        </div>
+
         <DataTable />
       </div>
+
       <Footer />
     </>
   );
--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -6,6 +6,7 @@ const DataTable = () => {
                     <tr>
                         <th>Data</th>
                         <th>Curso</th>
+                        <th>Escola</th>
                         <th>Quantidade de módulos</th>
                         <th>Horas por módulo</th>
                         <th>Total de horas</th>
@@ -15,70 +16,80 @@ const DataTable = () => {
                     <tr>
                         <td>10/08/2021</td>
                         <td>Formação certificação java</td>
-                        <td>6</td>
+                        <td>ALURA</td>
+                        <td className="alinhamento">6</td>
                         <td>90hs</td>
                         <td>90.00</td>
                     </tr>
                     <tr>
                         <td>24/08/2021</td>
                         <td>O básico de java</td>
-                        <td>1</td>
+                        <td>ALURA</td>
+                        <td className="alinhamento">1</td>
                         <td>8hs</td>
                         <td>8.00</td>
                     </tr>
                     <tr>
                         <td>25/08/2021</td>
                         <td>Tipos de Dados</td>
-                        <td>1</td>
+                        <td>ALURA</td>
+                        <td className="alinhamento">1</td>
                         <td>8hs</td>
                         <td>8.00</td>
                     </tr>
                     <tr>
                         <td>26/08/2021</td>
                         <td>Operadores</td>
-                        <td>1</td>
+                        <td>ALURA</td>
+                        <td className="alinhamento">1</td>
                         <td>8hs</td>
                         <td>8.00</td>
                     </tr>
                     <tr>
                         <td>27/08/2021</td>
                         <td>Criando e usando Array</td>
-                        <td>1</td>
+                        <td>ALURA</td>
+                        <td className="alinhamento">1</td>
                         <td>8hs</td>
                         <td>8.00</td>
                     </tr>
                     <tr>
                         <td>29/08/2021</td>
                         <td>Laços</td>
-                        <td>1</td>
+                        <td>ALURA</td>
+                        <td className="alinhamento">1</td>
                         <td>8hs</td>
                         <td>8.00</td>
                     </tr>
                     <tr>
                         <td>31/08/2021</td>
                         <td>Métodos e encapsulamento</td>
-                        <td>1</td>
+                        <td>ALURA</td>
+                        <td className="alinhamento">1</td>
                         <td>8hs</td>
                         <td>8.00</td>
                     </tr>
                     <tr>
                         <td>01/09/2021</td>
                         <td>Relações entre classes</td>
-                        <td>1</td>
+                        <td>ALURA</td>
+                        <td className="alinhamento">1</td>
                         <td>8hs</td>
                         <td>8.00</td>
                     </tr>
                     <tr>
                         <td>01/09/2021</td>
                         <td>Lidando com exceções</td>
-                        <td>1</td>
+                        <td>ALURA</td>
+                        <td className="alinhamento">1</td>
                         <td>8hs</td>
                         <td>8.00</td>
                     </tr>
                     <tr>
                         <td>01/09/2021</td>
                         <td>Conteúdo além da prova</td>
-                        <td>1</td>
+                        <td>ALURA</td>
+                        <td className="alinhamento">1</td>
                         <td>10hs</td>
                         <td>10.00</td>
                     </tr>
@@ -86,13 +97,15 @@ const DataTable = () => {
                         <td>-</td>
                         <td>-</td>
                         <td>-</td>
+                        <td className="alinhamento">-</td>
                         <td>-</td>
                         <td>-</td>
                     </tr>
                     <tr>
                         <td>05/09/2021</td>
                         <td>Formação Vue.Js</td>
-                        <td>4</td>
+                        <td>ALURA</td>
+                        <td className="alinhamento">4</td>
                         <td>120hs</td>
                         <td>120.0</td>
                     </tr>
